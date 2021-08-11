65cb80383e0f70bcea8c506fa887f52e03de06ec
firefu-mc <firefu-mc@example.com>
2021-08-11 19:32:08 +0100

    move select_directory, select_bedrock_directory, get_bedrock_worlds_dir to mcmuncher.common package

---
+++ mcfirefu/saves.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from pathlib import Path
+import os
+import sys
+import mcfirefu
+
+def get_bedrock_worlds_dir():
+    if sys.platform != "win32":
+        raise Exception("get_bedrock_worlds_dir is only available on Windows OS")
+
+    return os.path.join(
+        os.getenv("LOCALAPPDATA"),
+        "Packages",
+        "Microsoft.MinecraftUWP_8wekyb3d8bbwe",
+        "LocalState",
+        "games",
+        "com.mojang",
+        "minecraftWorlds",
+    )
+
+def select_bedrock_directory():
+    """
+    Loosely based on amulet_map_editor/api/wx/ui/select_world.py
+    in project https://github.com/Amulet-Team/Amulet-Map-Editor/
+
+    Prompts user for an installed world in users's Bedrock worlds directory.
+    Returns directory path as string.
+    """
+    worlds_dir = Path(get_bedrock_worlds_dir())
+    world_formats = []
+    for item in worlds_dir.iterdir():
+        if os.path.isdir(item):
+            format = None
+            try:
+                world_formats.append(amulet.load_format(item))
+            except FormatError as e:
+                warn(f"Could not find loader for {item} {e}")
+            except Exception:
+                warn(f"Error loading format wrapper for {item} {traceback.format_exc()}")
+
+    world_formats = list(reversed(sorted(world_formats, key=lambda f: f.last_played)))
+    counter = 0
+    for item in world_formats:
+        # add 1 to counter so we're not showing the user a zero-indexed list
+        print("[{:d}] {:s} - {:s}".format(counter+1, datetime.utcfromtimestamp(int(item.last_played)).strftime('%Y-%m-%d %H:%M:%S'), item.level_name))
+        counter+=1
+    select = int(input("Enter the number of the world you want to edit: "))
+    # subtract 1 from input
+    world = world_formats[select-1]
+    print("Selected: [{:d}] {:s}".format(select, world.level_name))
+
+    return world.path
+
+def select_directory():
+    if mcfirefu.args.directory:
+        return mcfirefu.args.directory
+    elif mcfirefu.args.bedrock:
+        return select_bedrock_directory()
+    else:
+        raise Exception("Unknown type of directory to search")
--- mcmuncher.py
+++ mcmuncher.py
@@ -1,73 +1,18 @@
 #!/usr/bin/env python
-from datetime import datetime
-import os
-from pathlib import Path
+
 import re
 import sys
 import amulet
 import mcfirefu
+import mcfirefu.saves
 from mcfirefu import warn, verbose
+from mcfirefu.pathtype import PathType
 
 dimensions = {
     "overworld": "minecraft:overworld",
     "nether": "minecraft:the_nether",
     "end":  "minecraft:the_end",
 }
-
-def get_bedrock_worlds_dir():
-    if sys.platform != "win32":
-        raise Exception("get_bedrock_worlds_dir is only available on Windows OS")
-
-    return os.path.join(
-        os.getenv("LOCALAPPDATA"),
-        "Packages",
-        "Microsoft.MinecraftUWP_8wekyb3d8bbwe",
-        "LocalState",
-        "games",
-        "com.mojang",
-        "minecraftWorlds",
-    )
-
-def select_bedrock_directory():
-    """
-    Loosely based on amulet_map_editor/api/wx/ui/select_world.py
-    in project https://github.com/Amulet-Team/Amulet-Map-Editor/
-
-    Prompts user for an installed world in users's Bedrock worlds directory.
-    Returns directory path as string.
-    """
-    worlds_dir = Path(get_bedrock_worlds_dir())
-    world_formats = []
-    for item in worlds_dir.iterdir():
-        if os.path.isdir(item):
-            format = None
-            try:
-                world_formats.append(amulet.load_format(item))
-            except FormatError as e:
-                warn(f"Could not find loader for {item} {e}")
-            except Exception:
-                warn(f"Error loading format wrapper for {item} {traceback.format_exc()}")
-
-    world_formats = list(reversed(sorted(world_formats, key=lambda f: f.last_played)))
-    counter = 0
-    for item in world_formats:
-        # add 1 to counter so we're not showing the user a zero-indexed list
-        print("[{:d}] {:s} - {:s}".format(counter+1, datetime.utcfromtimestamp(int(item.last_played)).strftime('%Y-%m-%d %H:%M:%S'), item.level_name))
-        counter+=1
-    select = int(input("Enter the number of the world you want to edit: "))
-    # subtract 1 from input
-    world = world_formats[select-1]
-    print("Selected: [{:d}] {:s}".format(select, world.level_name))
-
-    return world.path
-
-def select_directory():
-    if mcfirefu.args.directory:
-        return mcfirefu.args.directory
-    elif mcfirefu.args.bedrock:
-        return select_bedrock_directory()
-    else:
-        raise Exception("Unknown type of directory to search")
 
 def key_from_coord_tuple(coords) -> "x,z":
     """
@@ -203,7 +148,7 @@ def trim_chunks(level, dimension, coords_file):
 def main():
     mcfirefu.parse_args();
 
-    directory = select_directory()
+    directory = mcfirefu.saves.select_directory()
     level = amulet.load_level(directory)
 
     if mcfirefu.args.keep_overworld:
