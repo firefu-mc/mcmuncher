129ffd7d04d49e5e7bd4b1bd568a13e5c599ae89
firefu-mc <firefu-mc@example.com>
2021-08-11 20:11:14 +0100

    move all coord functions to new mcmuncher.coord_utils package

---
+++ mcfirefu/coord_utils.py
@@ -0,0 +1,94 @@
+import re
+import sys
+from mcfirefu import verbose
+
+def key_from_coord_tuple(coords) -> "x,z":
+    """
+    Returns a string usable as a dict key,
+    representing a [x,z] coord tuple
+    """
+    return str(coords[0]) + "," + str(coords[1])
+
+def get_coords_from_file(file) -> "[[x,z],[x,z],...]":
+    """
+    Takes a path to a coordinate config file for a dimension.
+    Returns to the list of coordinate rectangles that shouldn't be deleted
+    """
+    coords = []
+    valid_rectangle = re.compile("^(-?\d+),(-?\d+)\s+(-?\d+),(-?\d+)$")
+
+    for line in file:
+        line = line.lstrip()
+        if line.startswith("#"):
+            # ignore comments
+            continue
+        line = line.rstrip()
+        m = valid_rectangle.match(line)
+        if m:
+            coords.append([
+                [
+                    int(m.group(1)),
+                    int(m.group(2)),
+                ],
+                [
+                    int(m.group(3)),
+                    int(m.group(4)),
+                ],
+            ])
+        else:
+            warn("Line didn't match expected format: ", line)
+            sys.exit(1)
+
+    return coords
+
+def convert_coords_to_chunk_coords(coords) -> "[x,z]":
+    """
+    Converts a block coordinate [x,z] tuple into a chunk coordinate tuple [x,z]
+    """
+    x = coords[0] >> 4
+    z = coords[1] >> 4
+    return[x,z]
+
+def expand_coord_corners(coords) -> "[[x,z],[x,z],...]":
+    """
+    Takes a list of 2 coordinate tuples [[x1,z1],[x2,z2]] corresponding to
+    any 2 opposite corners of a rectangle.
+    Expands this list to include all contained coordinates within this
+    rectangle.
+    Returns a list of coordinate tuples
+    """
+    x1 = min(coords[0][0], coords[1][0])
+    x2 = max(coords[0][0], coords[1][0])
+
+    z1 = min(coords[0][1], coords[1][1])
+    z2 = max(coords[0][1], coords[1][1])
+
+    verbose("Corner coords\n", coords)
+
+    all_coords=[]
+    for x in range(x1, x2+1): # range stops 1 short!
+        for z in range(z1, z2+1): # range stops 1 short!
+            all_coords.append([x, z])
+
+    verbose("Expands to\n", all_coords)
+
+    return all_coords
+
+def convert_coord_list_to_chunk_coords(coord_corners):
+    chunk_corners = []
+    for i in coord_corners:
+        chunk_corners.append([
+            convert_coords_to_chunk_coords(i[0]),
+            convert_coords_to_chunk_coords(i[1]),
+        ])
+
+    keep_chunks = []
+    for i in chunk_corners:
+        keep_chunks.extend(expand_coord_corners(i))
+
+    # remove duplicate pairs
+    seen_chunk = dict()
+    for i in keep_chunks:
+        seen_chunk.update({key_from_coord_tuple(i): i})
+
+    return list(seen_chunk.values())
--- mcmuncher.py
+++ mcmuncher.py
@@ -1,112 +1,17 @@
 #!/usr/bin/env python
-
-import re
-import sys
 import amulet
 import mcfirefu
 import mcfirefu.saves
+import mcfirefu.coord_utils
 from mcfirefu import warn, verbose
-from mcfirefu.pathtype import PathType
-
-def key_from_coord_tuple(coords) -> "x,z":
-    """
-    Returns a string usable as a dict key,
-    representing a [x,z] coord tuple
-    """
-    return str(coords[0]) + "," + str(coords[1])
-
-def get_coords_from_file(file) -> "[[x,z],[x,z],...]":
-    """
-    Takes a path to a coordinate config file for a dimension.
-    Returns to the list of coordinate rectangles that shouldn't be deleted
-    """
-    coords = []
-    valid_rectangle = re.compile("^(-?\d+),(-?\d+)\s+(-?\d+),(-?\d+)$")
-
-    for line in file:
-        line = line.lstrip()
-        if line.startswith("#"):
-            # ignore comments
-            continue
-        line = line.rstrip()
-        m = valid_rectangle.match(line)
-        if m:
-            coords.append([
-                [
-                    int(m.group(1)),
-                    int(m.group(2)),
-                ],
-                [
-                    int(m.group(3)),
-                    int(m.group(4)),
-                ],
-            ])
-        else:
-            warn("Line didn't match expected format: ", line)
-            sys.exit(1)
-
-    return coords
-
-def convert_coords_to_chunk_coords(coords) -> "[x,z]":
-    """
-    Converts a block coordinate [x,z] tuple into a chunk coordinate tuple [x,z]
-    """
-    x = coords[0] >> 4
-    z = coords[1] >> 4
-    return[x,z]
-
-def expand_coord_corners(coords) -> "[[x,z],[x,z],...]":
-    """
-    Takes a list of 2 coordinate tuples [[x1,z1],[x2,z2]] corresponding to
-    any 2 opposite corners of a rectangle.
-    Expands this list to include all contained coordinates within this
-    rectangle.
-    Returns a list of coordinate tuples
-    """
-    x1 = min(coords[0][0], coords[1][0])
-    x2 = max(coords[0][0], coords[1][0])
-
-    z1 = min(coords[0][1], coords[1][1])
-    z2 = max(coords[0][1], coords[1][1])
-
-    verbose("Corner coords\n", coords)
-
-    all_coords=[]
-    for x in range(x1, x2+1): # range stops 1 short!
-        for z in range(z1, z2+1): # range stops 1 short!
-            all_coords.append([x, z])
-
-    verbose("Expands to\n", all_coords)
-
-    return all_coords
-
-
-def convert_coord_list_to_chunk_coords(coord_corners):
-    chunk_corners = []
-    for i in coord_corners:
-        chunk_corners.append([
-            convert_coords_to_chunk_coords(i[0]),
-            convert_coords_to_chunk_coords(i[1]),
-        ])
-
-    keep_chunks = []
-    for i in chunk_corners:
-        keep_chunks.extend(expand_coord_corners(i))
-
-    # remove duplicate pairs
-    seen_chunk = dict()
-    for i in keep_chunks:
-        seen_chunk.update({key_from_coord_tuple(i): i})
-
-    return list(seen_chunk.values())
 
 def trim_chunks(level, dimension, coords_file):
     warn("Dimension: ", dimension)
-    coords_from_file = get_coords_from_file(coords_file)
+    coords_from_file = mcfirefu.coord_utils.get_coords_from_file(coords_file)
 
     verbose("Coords from file\n", coords_from_file)
 
-    keep_chunks = convert_coord_list_to_chunk_coords(coords_from_file)
+    keep_chunks = mcfirefu.coord_utils.convert_coord_list_to_chunk_coords(coords_from_file)
 
     verbose("Max possible number of chunks to keep: ", len(keep_chunks))
 
@@ -117,12 +22,12 @@ def trim_chunks(level, dimension, coords_file):
     # dict of world chunks keyed on stringified coords
     world_chunks = dict()
     for i in world_chunks_list:
-        world_chunks.update({key_from_coord_tuple(i): i})
+        world_chunks.update({mcfirefu.coord_utils.key_from_coord_tuple(i): i})
 
     warn("Number of chunks in world file: ", len(world_chunks))
 
     for i in keep_chunks:
-        world_chunks.pop(key_from_coord_tuple(i), None)
+        world_chunks.pop(mcfirefu.coord_utils.key_from_coord_tuple(i), None)
 
     # everything remaining in dict needs to be deleted
     verbose("Going to delete number of chunks: ", len(world_chunks))
